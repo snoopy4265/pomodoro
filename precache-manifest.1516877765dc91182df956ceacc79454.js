d81bc5261e63e3b36edecc83946ddfe3b2d9febc
Michelle Wu <snoopy4265@gmail.com>
2019-07-23 21:08:39 +0200

    Updates

---
+++ precache-manifest.1516877765dc91182df956ceacc79454.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49f774dff85f4297d5ef499d9de21c74",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "958a6933492b1ebb8577",
+    "url": "/pomodoro/static/css/main.0eac549a.chunk.css"
+  },
+  {
+    "revision": "7685573bbbdc0ba85f9a",
+    "url": "/pomodoro/static/js/2.2a3879e5.chunk.js"
+  },
+  {
+    "revision": "958a6933492b1ebb8577",
+    "url": "/pomodoro/static/js/main.6109da80.chunk.js"
+  },
+  {
+    "revision": "8d8194bde761299ff809",
+    "url": "/pomodoro/static/js/runtime~main.5f1f8454.js"
+  },
+  {
+    "revision": "f70154d3268f2dbba158e6e11cfc462e",
+    "url": "/pomodoro/static/media/dot.f70154d3.svg"
+  }
+]);
