50257408cec221dd54b2fd3ccb9c4bcbad96ea13
Michelle Wu <snoopy4265@gmail.com>
2019-07-24 13:34:34 +0200

    Remove debug time and run deploy

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -6,7 +6,7 @@ class Timer extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-        time: 10, /* seconds */
+        time: 1500, /* seconds */
         start: false,
         percentage: 0,
       };
